bef65a666300322fb236793f38137bfc0fb9c933
t <v@buterin.com>
2024-10-10 13:51:30 +0800

    use esbuild as bundler

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const {ESBuildPlugin, ESBuildMinifyPlugin} = require('esbuild-loader')
 
 exports.onCreateWebpackConfig = ({ actions, loaders, stage, getConfig }) => {
   const config = getConfig();
@@ -24,9 +25,21 @@ exports.onCreateWebpackConfig = ({ actions, loaders, stage, getConfig }) => {
           {
             test: /bad-module/,
             use: loaders.null(),
+            loader: 'esbuild-loader',
           },
         ],
       },
+      optimization:{
+        minimize: true,
+        minimizer: [
+            new ESBuildMinifyPlugin({
+                target: 'es2015'
+            })
+        ]
+    },
+    plugins: [
+        new ESBuildPlugin()
+    ]
     });
   }
 };
